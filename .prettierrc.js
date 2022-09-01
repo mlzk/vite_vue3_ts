8eb1b04ac40f8ffed74ed0d7f838f6caef0601f4
liuzikang <zikang.liu@waytous.com>
2022-09-01 15:32:37 +0800

    add

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,18 @@
 module.exports = {
   tabWidth: 2,
+  // tab缩进大小,默认为2
   jsxSingleQuote: true,
+  // jsx 单引号
   jsxBracketSameLine: true,
+  //JSX尾标>签闭合位置 为true标识在同一行，false换行
   printWidth: 100,
+  //每行最多多少个字符换行
   singleQuote: true,
+  //使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
   semi: false,
+  // 使用分号
   endOfLine: 'auto',
+  //换行符
   overrides: [
     {
       files: '*.json',
@@ -15,4 +22,7 @@ module.exports = {
     },
   ],
   arrowParens: 'always',
+  // 箭头函数参数括号 默认avoid 可选 avoid| always
+  // avoid 能省略括号的时候就省略 例如x => x
+  // always 总是有括号
 }
--- src/components/layout/columnLayout/Index.vue
+++ src/components/layout/columnLayout/Index.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="column-layout-container">
     <div class="column-layout-header-container">这是header</div>
@@ -17,46 +16,43 @@ body {
   position: relative;
   box-sizing: border-box;
   height: 100vh;
-  padding: 0;
   overflow: hidden;
-  font-family: PingFang SC, Arial, Microsoft YaHei, sans-serif;
+  padding: 0;
   font-size: 14px;
+  font-family: 'PingFang SC', Arial, 'Microsoft YaHei', sans-serif;
   background: #f6f8f9;
   -webkit-font-smoothing: antialiased;
 }
-
 .column-layout-container {
   position: relative;
   width: 100%;
   height: 100%;
-  background-color: #fefefe;
   padding-top: 110px;
-
+  background-color: #fefefe;
   .column-layout-slider-menus-container {
-    width: 220px;
-    height: 100%;
-    background-color: #fb5050;
-    padding-top: 110px;
     position: fixed;
-    left: 0;
     top: 0;
     bottom: 0;
+    left: 0;
+    width: 220px;
+    height: 100%;
+    padding-top: 110px;
+    background-color: #fb5050;
   }
-
   .column-layout-header-container {
-    width: 100%;
     position: fixed;
     top: 0;
     right: 0;
     z-index: 1998;
-    background-color: #25baff;
+    width: 100%;
     min-height: 110px;
+    background-color: #25baff;
     box-shadow: 0 1px 4px rgb(0 21 41 / 8%);
   }
-
   .column-content {
-    margin-left: 220px;
     width: auto;
+    width: 10px;
+    margin-left: 220px;
     padding: 20px;
     overflow: hidden;
     transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1), border 0s, color 0.1s, font-size 0s;
