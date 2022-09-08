2cd99c04a609912bc4a93252df9f25379cc442be
liuzikang <zikang.liu@waytous.com>
2022-09-08 14:30:17 +0800

    test

--- commitlint.config.js
+++ commitlint.config.js
@@ -15,5 +15,12 @@ module.exports = {
   extends: ['cz'],
   rules: {
     'type-enum': [2, 'always', types],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72],
   },
 }
