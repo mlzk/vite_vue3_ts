424ed89bff8f1b30475a6fb2b5934c3ba47bbdb5
liuzikang <zikang.liu@waytous.com>
2022-09-08 14:46:44 +0800

    [chore](配置文件): 更新配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -10,17 +10,5 @@
 // revert：回滚某个更早之前的提交
 // chore：不属于以上类型的其他类型(日常事务)
 const types = require('commitlint-config-cz/lib/types')()
-
-module.exports = {
-  extends: ['@commitlint/config-conventional', 'cz'],
-  rules: {
-    'type-enum': [2, 'always', types],
-    'type-case': [0],
-    'type-empty': [0],
-    'scope-empty': [0],
-    'scope-case': [0],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72],
-  },
-}
+const config = require('commitlint-config-cz/lib/config')()
+module.exports = config
