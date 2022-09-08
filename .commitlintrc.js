d55d1524a6933e374511ed4a7e5a25ae5decdb40
liuzikang <zikang.liu@waytous.com>
2022-09-08 17:14:32 +0800

    feat(components): 更新了基础配置

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,5 @@
+// const scopes = require('commitlint-config-cz/lib/scopes')();
+const types = require('commitlint-config-cz/lib/types')();
 module.exports = {
 	rules: {
 		'body-leading-blank': [1, 'always'],
@@ -14,22 +16,14 @@ module.exports = {
 		'subject-full-stop': [2, 'never', '.'],
 		'type-case': [2, 'always', 'lower-case'],
 		'type-empty': [2, 'never'],
+		// 'scope-enum': [
+		// 	2,
+		// 	'always',
+		// 	scopes],
 		'type-enum': [
 			2,
 			'always',
-			[
-				'build',
-				'chore',
-				'ci',
-				'docs',
-				'feat',
-				'fix',
-				'perf',
-				'refactor',
-				'revert',
-				'style',
-				'test',
-			],
+			types
 		],
 	},
 
