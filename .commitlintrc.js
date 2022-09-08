5adddfb5bf4de6cc21103d971f41d58759d10cbf
liuzikang <zikang.liu@waytous.com>
2022-09-08 11:29:34 +0800

    test

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -12,19 +12,6 @@
 
 module.exports = {
   extends: [
-    "@commitlint/config-conventional",
     'cz'
   ],
-  // rules: {
-  //   'type-enum': [2, 'always', [
-  //     'build','ci','perf', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert'
-  //    ]],
-  //   'type-case': [0],
-  //   'type-empty': [0],
-  //   'scope-empty': [0],
-  //   'scope-case': [0],
-  //   'subject-full-stop': [0, 'never'],
-  //   'subject-case': [0, 'never'],
-  //   'header-max-length': [0, 'always', 72]
-  // }
 };
